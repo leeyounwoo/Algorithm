fad8eea5506401133463cc71e6d70a89b3ef93a4
Younwoo Lee <leeyw2709@gmail.com>
2021-12-14 07:26:46 +0900

    Solve baekjoon 3009 네 번째 점

--- BAEKJOON/09. 기본 수학 2/s8.py
+++ BAEKJOON/09. 기본 수학 2/s8.py
@@ -1,2 +1,13 @@
-a, b = map(int, input().split())
-print(a / b)
+ans_x = []
+ans_y = []
+for i in range(3):
+    x, y = map(int, input().split())
+    if x not in ans_x:
+        ans_x.append(x)
+    else:
+        ans_x.remove(x)
+    if y not in ans_y:
+        ans_y.append(y)
+    else:
+        ans_y.remove(y)
+print('{} {}'.format(ans_x[0], ans_y[0]))
