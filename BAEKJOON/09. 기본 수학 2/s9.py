c88b66c94edb5f7a9d553526b294907ca3ed80e6
Younwoo Lee <leeyw2709@gmail.com>
2021-12-14 07:32:57 +0900

    Solve baekjoon 4153 직각삼각형

--- BAEKJOON/09. 기본 수학 2/s9.py
+++ BAEKJOON/09. 기본 수학 2/s9.py
@@ -1,6 +1,9 @@
-a, b = map(int, input().split())
-print(a + b)
-print(a - b)
-print(a * b)
-print(a // b)
-print(a % b)
+while True:
+    lengths = list(map(int, input().split()))
+    if not sum(lengths):
+        break
+    longest = lengths.pop(lengths.index(max(lengths)))
+    if longest ** 2 == lengths[0] ** 2 + lengths[1] ** 2:
+        print("right")
+    else:
+        print("wrong")
