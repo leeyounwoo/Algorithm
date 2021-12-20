5fae0c4859b8912dd363b39be3db7a3c4b1a5275
Younwoo Lee <leeyw2709@gmail.com>
2021-12-20 10:42:14 +0900

    Solve baekjoon 7568 덩치

---
+++ BAEKJOON/클래스별로 풀기/claas 2/7568_덩치.py
@@ -0,0 +1,12 @@
+n = int(input())
+people = [list(map(int, input().split())) for _ in range(n)]
+ans = []
+for i in range(n):
+    rank = 1
+    for j in range(n):
+        if i == j:
+            continue
+        if people[i][0] < people[j][0] and people[i][1] < people[j][1]:
+            rank += 1
+    ans.append(str(rank))
+print(' '.join(ans))
