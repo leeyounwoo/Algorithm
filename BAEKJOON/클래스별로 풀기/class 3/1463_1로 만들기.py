54692fe28d05b5c91c9e11daccb84b6ceb3f4aa5
Younwoo Lee <leeyw2709@gmail.com>
2021-12-24 13:39:26 +0900

    Solve baekjoon class 3 1463 1로 만들기

---
+++ BAEKJOON/클래스별로 풀기/class 3/1463_1로 만들기.py
@@ -0,0 +1,22 @@
+import sys
+
+def bfs(n, cnt):
+    global ans
+    if cnt >= ans:
+        return
+    if n == 1:
+        if cnt < ans:
+            ans = cnt
+        return
+    else:
+        if n % 3 == 0:
+            bfs(n//3, cnt+1)
+        if n % 2 == 0:
+            bfs(n//2, cnt+1)
+        bfs(n-1, cnt+1)
+        
+sys.stdin = open('input.txt')
+n = int(input())
+ans = n
+bfs(n, 0)
+print(ans)
