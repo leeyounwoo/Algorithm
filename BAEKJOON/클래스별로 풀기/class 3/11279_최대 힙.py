c2245c82b821884ef0b17b81b51e8e4b56253953
Younwoo Lee <leeyw2709@gmail.com>
2021-12-31 22:39:59 +0900

    Solve baekjoon class 3 1927 최소 힙

---
+++ BAEKJOON/클래스별로 풀기/class 3/11279_최대 힙.py
@@ -0,0 +1,61 @@
+import sys
+
+
+class MaxHeap:
+    def __init__(self):
+        self.data = [0]
+
+    def insert(self, item):
+        self.data.append(item)
+        i = len(self.data) - 1
+        while i > 1:
+            if self.data[i] > self.data[i//2]:
+                self.data[i], self.data[i//2] = self.data[i//2], self.data[i]
+                i //= 2
+            else:
+                break
+
+    def remove(self):
+        if len(self.data) > 1:
+            self.data[1], self.data[-1] = self.data[-1], self.data[1]
+            data = self.data.pop(-1)
+            self.maxHeapify(1)
+        else:
+            data = 0
+        return data
+
+    def maxHeapify(self, i):
+        left = 2 * i
+        right = (2 * i) + 1
+        biggest = i
+        # 왼쪽 자식이 존재하는지, 그리고 왼쪽 자식의 (키) 값이 부모보다 더 큰지 판단.
+        if left < len(self.data) and self.data[i] < self.data[left]:
+            biggest = left
+        # 오른쪽 자식이 존재하는지, 그리고 지금까지 최대값보다 오른쪽 자식의 값이 더 큰지 판단
+        if right < len(self.data) and self.data[biggest] < self.data[right]:
+            biggest = right
+        # 오른쪽 자식 혹은 왼쪽 자식이 더 큰 경우, 값 교환 + 재귀호출
+        if biggest != i:
+            self.data[i], self.data[biggest] = self.data[biggest], self.data[i]
+            self.maxHeapify(biggest)
+
+sys.stdin = open('input.txt')
+n = int(input())
+heap = MaxHeap()
+for _ in range(n):
+    num = int(sys.stdin.readline().rstrip())
+    # print('num', num)
+    if num == 0:
+        if not heap.data:
+            print(0)
+        else:
+            print(heap.remove())
+
+    else:
+        heap.insert(num)
+        # if not heap:
+        #     heap.append(num)
+        # else:
+        #     heap.append(num)
+
+
