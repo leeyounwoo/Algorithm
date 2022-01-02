9ad7a51b547ba41b79dd46c4cc56be11379c70e6
Younwoo Lee <leeyw2709@gmail.com>
2022-01-02 17:21:48 +0900

    Solve baekjoon class 3 9375 패션왕 신혜빈

--- BAEKJOON/클래스별로 풀기/class 3/9375_패션왕 신해빈.py
+++ BAEKJOON/클래스별로 풀기/class 3/9375_패션왕 신해빈.py
@@ -1,5 +1,4 @@
 import sys
-from itertools import combinations
 
 sys.stdin = open('input.txt')
 for T in range(int(input())):
@@ -8,8 +7,10 @@ for T in range(int(input())):
     for _ in range(n):
         name, key = map(str, sys.stdin.readline().rstrip().split())
         if key not in clothes:
-            clothes[key] = [name]
-        elif name not in clothes[key]:
-            clothes[key].append(name)
-    clothes_keys = list(clothes.keys())
-    print(clothes)
+            clothes[key] = 1
+        else:
+            clothes[key] += 1
+    ans = 1
+    for key in clothes.keys():
+        ans *= clothes[key] + 1
+    print(ans-1)
