e7a831b9bdb8a804e47b6e0c3eb9dd6dc38df298
Younwoo Lee <leeyw2709@gmail.com>
2022-01-09 19:48:22 +0900

    Solve baekjoon class 3 6064 카잉 달력

--- BAEKJOON/클래스별로 풀기/class 3/6064_카잉 달력.py
+++ BAEKJOON/클래스별로 풀기/class 3/6064_카잉 달력.py
@@ -1,5 +1,19 @@
 import sys
 
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def num(M, N, x, y):
+    while x <= M * N:
+        if (x - y) % N == 0:
+            return x
+        x += M
+    else:
+        return -1
+
 sys.stdin = open('input.txt')
 for _ in range(int(input())):
-    m, n, x, y = map(int, input().split())
+    M, N, x, y = map(int, input().split())
+    print(num(M, N, x, y))
