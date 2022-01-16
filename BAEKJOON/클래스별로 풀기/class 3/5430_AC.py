bf2c59173d25ef13d105c5de781c1e7012114f29
Younwoo Lee <leeyw2709@gmail.com>
2022-01-16 19:57:38 +0900

    Solve baekjoon class 3 5430 AC

--- BAEKJOON/클래스별로 풀기/class 3/5430_AC.py
+++ BAEKJOON/클래스별로 풀기/class 3/5430_AC.py
@@ -8,26 +8,31 @@ def input():
 sys.stdin = open('input.txt')
 for T in range(int(input())):
     func = list(input())
-    # print(func)
     n = int(input())
     strings = input()
 
     array = deque(strings[1:-1].split(','))
-    # print(T, array)
+    flag = True
     for i in range(len(func)):
-        # print(func[i])
         if func[i] == 'R':
-            array.reverse()
-            # print(array)
+            if flag:
+                flag = False
+            else:
+                flag = True
+
         else:
-            # print('123')
             if len(array) <= 0 or array[0] == '':
-                # print('45')
                 print('error')
                 break
             else:
-                array.popleft()
+                if flag:
+                    array.popleft()
+                else:
+                    array.pop()
+
     else:
+        if not flag:
+            array.reverse()
         print('[', end='')
         for i in range(len(array)):
             print(array[i], end='')
