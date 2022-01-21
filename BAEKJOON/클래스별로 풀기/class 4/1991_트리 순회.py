8163606a74eb71e190fbb36bfe7939b26c716c99
Younwoo Lee <leeyw2709@gmail.com>
2022-01-22 05:00:00 +0900

    Solve baekjoon class 4 1991 트리 순회

---
+++ BAEKJOON/클래스별로 풀기/class 4/1991_트리 순회.py
@@ -0,0 +1,54 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+def preorder(now):
+    global pattern_preorder
+    pattern_preorder += now
+    if graph[now]['left'] != '.':
+        preorder(graph[now]['left'])
+    if graph[now]['right'] != '.':
+        preorder(graph[now]['right'])
+
+
+
+def inorder(now):
+    global pattern_inorder
+    if graph[now]['left'] != '.':
+        inorder(graph[now]['left'])
+    pattern_inorder += now
+    if graph[now]['right'] != '.':
+        inorder(graph[now]['right'])
+
+
+def postorder(now):
+    global pattern_postorder
+    if graph[now]['left'] != '.':
+        postorder(graph[now]['left'])
+    if graph[now]['right'] != '.':
+        postorder(graph[now]['right'])
+    pattern_postorder += now
+
+
+sys.stdin = open('input.txt')
+n = int(input())
+# print(ord('A'))
+graph = {chr(65+i): {'left': '', 'right': ''} for i in range(n)}
+for i in range(n):
+    node, left, right = map(str, input().split())
+    graph[node]['left'] = left
+    graph[node]['right'] = right
+
+pattern_preorder = ''
+preorder('A')
+print(pattern_preorder)
+
+pattern_inorder = ''
+inorder('A')
+print(pattern_inorder)
+
+pattern_postorder = ''
+postorder('A')
+print(pattern_postorder)
