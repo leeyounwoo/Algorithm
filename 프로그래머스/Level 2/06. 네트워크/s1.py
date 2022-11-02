6d5b54c6deb301d4498c0d116a7bf832d5400de6
Younwoo Lee <leeyw2709@gmail.com>
2022-11-02 19:18:39 +0900

    Solve Programmers level 2 06. 네트워크

---
+++ 프로그래머스/Level 2/06. 네트워크/s1.py
@@ -0,0 +1,34 @@
+def bfs(index):
+    global checked
+    global linked_list
+    for num in linked_list[index]:
+        if checked[num]:
+            continue
+        else:
+            checked[num] = True
+            bfs(num)
+    return
+
+def solution(n, computers):
+    global checked
+    global linked_list
+    checked = [False] * n
+    linked_list = []
+    for i in range(n):
+        temp = []
+        for j in range(len(computers[i])):
+            if computers[i][j] == 1 and i != j:
+                temp.append(j)
+        linked_list.append(temp)
+
+    answer = 0
+    for i in range(n):
+        if checked[i]:
+            continue
+        else:
+            answer += 1
+            bfs(i)
+    return answer
+
+
+print(solution(3, [[1,1,0], [1,1,0], [0,0,1]]))
