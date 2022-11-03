ae756bb4c3df0317e0ce6c88fd344da01475d6fc
Younwoo Lee <leeyw2709@gmail.com>
2022-11-03 23:33:45 +0900

    Solve Programmers level 3 06. 단어 변환

--- 프로그래머스/Level 3/06. 단어 변환/s1.py
+++ 프로그래머스/Level 3/06. 단어 변환/s1.py
@@ -1,17 +1,43 @@
-from heapq import heappush, nlargest, nsmallest
-def solution(operations):
-    answer = []
-    for operation in operations:
-        command, num = operation.split()
-        num = int(num)
-        if command == 'I':
-            heappush(answer, num)
-        else:
-            if num == 1:
-                answer = nlargest(len(answer), answer)[1:]
+def dfs(now, checked, flag):
+    global answer
+    for i in range(len(words1)):
+        # 이미 접근했던 단어이면 pass
+        if checked[i]:
+            continue
+
+        # 접근한 적이 없는 단어
+        same_cnt = 0
+        for j in range(len(now)):
+            if now[j] == words1[i][j]:
+                same_cnt += 1
+        # 한 글자만 다른 경우
+        if same_cnt == len(now) - 1:
+            # 원하는 단어인 경우
+            if words1[i] == target1:
+                if flag < answer:
+                    answer = flag
+                    return
             else:
-                answer = nsmallest(len(answer), answer)[1:]
-    if len(answer) == 0:
-        return [0, 0]
-    else:
-        return [nlargest(1, answer)[0], nsmallest(1, answer)[0]]
+                if flag + 1 == len(words1):
+                    return
+                checked[i] = True
+                dfs(words1[i], checked, flag + 1)
+        else:
+            continue
+def solution(begin, target, words):
+    global answer
+    global target1
+    global words1
+    target1 = target
+    words1 = words
+    answer = len(words)
+
+    checked = [False] * len(words)
+    dfs(begin, checked, 1)
+    if answer == len(words):
+        return 0
+
+    return answer
+
+print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
+print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log"]))
