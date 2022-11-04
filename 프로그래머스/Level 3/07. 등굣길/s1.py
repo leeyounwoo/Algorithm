f319e8f6bb4f7c3461a39df4c6621069f5339098
Younwoo Lee <leeyw2709@gmail.com>
2022-11-04 09:57:28 +0900

    Solve Programmers level 3 07. 등굣길

--- 프로그래머스/Level 3/07. 등굣길/s1.py
+++ 프로그래머스/Level 3/07. 등굣길/s1.py
@@ -1,17 +1,42 @@
-from heapq import heappush, nlargest, nsmallest
-def solution(operations):
-    answer = []
-    for operation in operations:
-        command, num = operation.split()
-        num = int(num)
-        if command == 'I':
-            heappush(answer, num)
-        else:
-            if num == 1:
-                answer = nlargest(len(answer), answer)[1:]
-            else:
-                answer = nsmallest(len(answer), answer)[1:]
-    if len(answer) == 0:
-        return [0, 0]
-    else:
-        return [nlargest(1, answer)[0], nsmallest(1, answer)[0]]
+def solution(m, n, puddles):
+    array = [[0] * m for _ in range(n)]
+    for i in range(n):
+        array[i][0] = 1
+    for j in range(m):
+        array[0][j] = 1
+
+    for puddle in puddles:
+        index_j, index_i = puddle
+        if index_i == 1:
+            for j in range(index_j, m):
+                array[0][j] = -1
+
+        if index_j == 1:
+            for i in range(index_i, n):
+                array[i][0] = -1
+        array[index_i-1][index_j-1] = -1
+
+    for i in range(1, n):
+        for j in range(1, m):
+            # 물에 잠긴 경우
+            if array[i][j] == -1:
+                continue
+            up = array[i-1][j]
+            left = array[i][j-1]
+            if up == -1 and left != -1:
+                array[i][j] = left
+            elif up != -1 and left == -1:
+                array[i][j] = up
+            elif up == -1 and left == -1:
+                array[i][j] = -1
+            elif up != -1 and left != -1:
+                array[i][j] = (up + left) % 1000000007
+
+    answer = array[n-1][m-1]
+    if answer == -1:
+        return 0
+
+    return array[n-1][m-1]
+
+
+print(solution(4, 3, [[1, 2], [2, 1]]))
