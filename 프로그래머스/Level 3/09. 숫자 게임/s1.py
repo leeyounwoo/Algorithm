8ef9a81e3349c0761ab0f18e4e77c639e44d5390
Younwoo Lee <leeyw2709@gmail.com>
2022-11-07 23:35:55 +0900

    Solve Programmers level 3 09. 숫자 게임

--- 프로그래머스/Level 3/09. 숫자 게임/s1.py
+++ 프로그래머스/Level 3/09. 숫자 게임/s1.py
@@ -1,17 +1,29 @@
-from heapq import heappush, nlargest, nsmallest
-def solution(operations):
-    answer = []
-    for operation in operations:
-        command, num = operation.split()
-        num = int(num)
-        if command == 'I':
-            heappush(answer, num)
+from heapq import heappush, nlargest, heappop
+
+def solution(A, B):
+    array_a = []
+    array_b = []
+    for num in A:
+        heappush(array_a, -num)
+    for num in B:
+        heappush(array_b, -num)
+
+    answer = 0
+    while len(array_a):
+        max_a = -1 * heappop(array_a)
+        max_b = -1 * heappop(array_b)
+        if max_b <= max_a:
+            heappush(array_b, -max_b)
         else:
-            if num == 1:
-                answer = nlargest(len(answer), answer)[1:]
-            else:
-                answer = nsmallest(len(answer), answer)[1:]
-    if len(answer) == 0:
-        return [0, 0]
-    else:
-        return [nlargest(1, answer)[0], nsmallest(1, answer)[0]]
+            answer += 1
+    # while len(array_a) and len(array_b):
+    #     max_a = heappop(array_a)
+    #     array_a = nlargest(len(array_a), array_a)[1:]
+    #     max_b = nlargest(1, array_b)[0]
+    #     if max_b > max_a:
+    #         array_b = nlargest(len(array_b), array_b)[1:]
+    #         answer += 1
+
+    return answer
+
+print(solution([5,1,3,7], [2,2,6,8]))
